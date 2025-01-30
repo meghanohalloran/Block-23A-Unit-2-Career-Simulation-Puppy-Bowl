53c05fcd2f9d8f167f5bce77ddd9ff436a243b65
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-01-30 09:42:25 -0700

    updated to fetch players

---
+++ index.js
@@ -0,0 +1,51 @@
+async function fetchPlayers() {
+try {
+    const response = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2501-ftb-et-web-ft/players');
+    const result = await response.json();
+    console.log(result);
+  } catch (err) {
+    console.error(err)('error fetching players', err);
+  }
+}
+  
+//   // {
+//     "success": true,
+//     "error": null,
+//     "data": {
+//         "players": [
+//             {
+//                 "id": 2,
+//                 "name": "Crumpet",
+//                 "breed": "American Staffordshire Terrier",
+//                 "status": "field",
+//                 "imageUrl": "http://r.ddmcdn.com/w_1012/s_f/o_1/cx_0/cy_0/cw_1012/ch_1518/APL/uploads/2019/12/Crumpet-PBXVI.jpg",
+//                 "createdAt": "2501-ftb-et-web-ft",
+//                 "updatedAt": "2021-06-15T16:21:14.103Z",
+//                 "teamId": 1,
+//                 "cohortId": 1
+//             },
+//             {
+//                 "id": 4,
+//                 "name": "Daphne",
+//                 "breed": "German Shepherd",
+//                 "status": "field",
+//                 "imageUrl": "http://r.ddmcdn.com/w_960/s_f/o_1/cx_25/cy_0/cw_960/ch_1440/APL/uploads/2020/01/Daphne-PBXVI.jpg",
+//                 "createdAt": "2021-06-15T16:21:14.103Z",
+//                 "updatedAt": "2021-06-15T16:21:14.103Z",
+//                 "teamId": 2,
+//                 "cohortId": 1
+//             },
+//             {
+//                 "id": 10,
+//                 "name": "Kenny",
+//                 "breed": "Golden Retriever / Boxer",
+//                 "status": "bench",
+//                 "imageUrl": "http://r.ddmcdn.com/w_1012/s_f/o_1/cx_0/cy_0/cw_1012/ch_1518/APL/uploads/2019/12/Kenny-PBXVI.jpg",
+//                 "createdAt": "2021-06-15T16:21:14.103Z",
+//                 "updatedAt": "2021-06-15T16:21:14.103Z",
+//                 "teamId": 2,
+//                 "cohortId": 1
+//             }
+//         ]
+//     }
+// }
